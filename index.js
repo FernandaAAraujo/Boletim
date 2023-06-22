a5990487e547f9a1d15cb512b7d56716938c387b
Fernanda Araujo <fealmeida20210@gmail.com>
2023-06-22 19:46:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+        var n1=Number(window.prompt(`Digite a primeira nota`))
+        var n2=Number(window.prompt(`Digite a segunda nota`))
+        var n3=Number(window.prompt(`Digite a terceira nota`))
+
+        var media=(n1+n2+n3)/3
+
+        
+
+        if (media>=5){
+            document.write(`A média é ${media}  Aprovado`)
+            
+        }
+
+        else  {
+            document.write(`A média é ${media} Reprovado`)
+        
+        }
+ 
+
+
+
